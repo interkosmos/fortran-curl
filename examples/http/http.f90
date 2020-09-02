b0c6a851e103dba00595bf9db86dd06c37ced29f
Philipp <pengel@hs-nb.de>
2020-09-02 20:51:02 +0200

    Reverted changes.

--- examples/http/http.f90
+++ examples/http/http.f90
@@ -24,7 +24,6 @@ contains
         !! This callback function might be called several times by libcurl,
         !! passing in more chunks of the response.
         use, intrinsic :: iso_c_binding, only: c_associated, c_f_pointer, c_ptr, c_size_t
-        use :: curl, only: c_f_str_ptr
         type(c_ptr),            intent(in), value :: ptr               !! C pointer to a chunk of the response.
         integer(kind=c_size_t), intent(in), value :: size              !! Always 1.
         integer(kind=c_size_t), intent(in), value :: nmemb             !! Size of the response chunk.
@@ -60,6 +59,31 @@ contains
         deallocate (tmp)
         response_callback = nmemb
     end function response_callback
+
+    subroutine c_f_str_ptr(c_str, f_str)
+        !! Utility routine that copies a C string, passed as a C pointer, to a
+        !! Fortran string.
+        use, intrinsic :: iso_c_binding, only: c_associated, c_char, c_f_pointer, c_null_char, c_ptr
+        type(c_ptr),      intent(in)           :: c_str
+        character(len=*), intent(out)          :: f_str
+        character(kind=c_char, len=1), pointer :: chars(:)
+        integer                                :: i
+
+        if (.not. c_associated(c_str)) then
+            f_str = ' '
+            return
+        end if
+
+        call c_f_pointer(c_str, chars, [ huge(0) ])
+        i = 1
+
+        do while (chars(i) /= c_null_char .and. i <= len(f_str))
+            f_str(i:i) = chars(i)
+            i = i + 1
+        end do
+
+        if (i < len(f_str)) f_str(i:) = ' '
+    end subroutine c_f_str_ptr
 end module callback
 
 program main
