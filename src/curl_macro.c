8ccd1c049af2165736f284fe4e20714e229d41c3
Philipp <pengel@hs-nb.de>
2024-01-01 19:46:06 +0100

    Minor changes.

--- src/curl_macro.c
+++ src/curl_macro.c
@@ -5,20 +5,24 @@
 extern "C" {
 #endif
 
-int curl_version_now(void);
+int curl_easy_setopt_c_char(CURL *, CURLoption option, char *);
+int curl_easy_setopt_c_funptr(CURL *, CURLoption option, void *);
 int curl_easy_setopt_c_int(CURL *, CURLoption option, int);
 int curl_easy_setopt_c_long(CURL *, CURLoption option, long);
 int curl_easy_setopt_c_ptr(CURL *, CURLoption option, void *);
-int curl_easy_setopt_c_char(CURL *, CURLoption option, char *);
-int curl_easy_setopt_c_funptr(CURL *, CURLoption option, void *);
-
-/* Wrapper function that provides access to the constant `CURLVERSION_NOW`. */
-int curl_version_now(void)
-{
-    return CURLVERSION_NOW;
-}
+int curl_version_now(void);
 
 /* Non-variadic wrappers to `curl_easy_setopt()`. */
+int curl_easy_setopt_c_char(CURL *curl, CURLoption option, char *value)
+{
+    return curl_easy_setopt(curl, option, value);
+}
+
+int curl_easy_setopt_c_funptr(CURL *curl, CURLoption option, void *value)
+{
+    return curl_easy_setopt(curl, option, value);
+}
+
 int curl_easy_setopt_c_int(CURL *curl, CURLoption option, int value)
 {
     return curl_easy_setopt(curl, option, value);
@@ -34,14 +38,10 @@ int curl_easy_setopt_c_ptr(CURL *curl, CURLoption option, void *value)
     return curl_easy_setopt(curl, option, value);
 }
 
-int curl_easy_setopt_c_char(CURL *curl, CURLoption option, char *value)
+/* Wrapper function that provides access to the constant `CURLVERSION_NOW`. */
+int curl_version_now(void)
 {
-    return curl_easy_setopt(curl, option, value);
-}
-
-int curl_easy_setopt_c_funptr(CURL *curl, CURLoption option, void *value)
-{
-    return curl_easy_setopt(curl, option, value);
+    return CURLVERSION_NOW;
 }
 
 #ifdef __cplusplus
